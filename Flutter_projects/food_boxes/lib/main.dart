53a24df82c7d88c199a99fccf60611e08751e133
Josh <jcborut@gmail.com>
2023-04-03 20:34:07 -0700

    Intial framework for food boxes app

---
+++ Flutter_projects/food_boxes/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Food Boxes",
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) {
+          return HomeScreen();
+        },
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        );
+      },
+    );
+  }
+}
---
+++ Flutter_projects/food_boxes/lib/screens/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  static const routeName = "/";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}

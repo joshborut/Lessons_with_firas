edc59126ecb51dd63ab1c6f2bd9d3e3ed0acc358
Josh <jcborut@gmail.com>
2023-04-03 20:53:35 -0700

    Created home screen's front end

--- Flutter_projects/food_boxes/lib/screens/home_screen.dart
+++ Flutter_projects/food_boxes/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +7,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Home Screen"),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle_outlined),
+          label: "Account",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.food_bank),
+          label: "Food Boxes",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.receipt),
+          label: "Tickets",
+        ),
+      ]),
     );
   }
 }

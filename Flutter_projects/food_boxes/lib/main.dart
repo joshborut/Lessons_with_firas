69049c9b556198e544f2e6a8f1a12c05a668cc73
Josh <jcborut@gmail.com>
2023-05-05 17:39:11 -0700

    Integrated & initialized firebase_core

--- Flutter_projects/food_boxes/lib/main.dart
+++ Flutter_projects/food_boxes/lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:food_boxes/firebase_options.dart';
 
 import 'routes.dart';
 import 'screens/auth_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 

69dff585fc29a73c77eb17e0e0629b337ee11ee4
MrJoshCode <jcborut@gmail.com>
2023-07-27 17:33:06 -0400

    Learned about webscraper pt2

--- Algorithms/lib/scraper.py
+++ Algorithms/lib/scraper.py
@@ -3,11 +3,29 @@ import requests
 
 URL = "https://pub.dev"
 
-response = requests.get(URL, timeout=5)
-soup = BeautifulSoup(response.text, "html.parser")
 
-for element in soup.findAll(attrs={"class": "mini-list-item"}):
-    page_extension = element.find("a")["href"]
-    if page_extension is not None:
-        print(URL + page_extension)
+def parse_main_page():
+    response = requests.get(URL, timeout=5)
+    soup = BeautifulSoup(response.text, "html.parser")
 
+    for element in soup.find_all(attrs={"class": "mini-list-item"}):
+        page_extension = element.find("a")["href"]
+        if page_extension is not None:
+            gather_dependencies(URL + page_extension)
+
+
+def gather_dependencies(url):
+    print(f"Popular packages: {url}")
+    print("Dependencies:")
+
+    response = requests.get(url, timeout=5)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    for element in soup.find_all(attrs={"class": "detail-info-box"}):
+        a_elements = element.find_all("a", title=True)
+        for a in a_elements:
+            print(a["href"])
+    print("\n")
+
+
+parse_main_page()

8c378ce71d825426b8105ef31b4325c9df72350a
MrJoshCode <jcborut@gmail.com>
2023-08-14 20:47:39 -0400

    Fixed envied bug & worked on establishing firebase

--- Flutter_projects/food_boxes/lib/env.dart
+++ Flutter_projects/food_boxes/lib/env.dart
@@ -5,11 +5,11 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'WEB_KEY')
-  static const webKey = _Env.webKey;
+  static final String webKey = _Env.webKey;
   @EnviedField(varName: 'ANDROID_KEY')
-  static const androidKey = _Env.androidKey;
+  static final String androidKey = _Env.androidKey;
   @EnviedField(varName: 'IOS_KEY')
-  static const iosKey = _Env.iosKey;
+  static final String iosKey = _Env.iosKey;
   @EnviedField(varName: 'MACOS_KEY')
-  static const macosKey = _Env.macosKey;
+  static final String macosKey = _Env.macosKey;
 }

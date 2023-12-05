7f0915bd2b7fb9c19610c171baeaa63ed2b65707
Josh <jcborut@gmail.com>
2023-12-04 21:53:44 -0700

    Improved creating tickets & calendar functionality

--- Flutter_projects/food_boxes/lib/widgets/schedule_page.dart
+++ Flutter_projects/food_boxes/lib/widgets/schedule_page.dart
@@ -4,32 +4,62 @@ import 'package:food_boxes/app_constants.dart';
 import 'package:food_boxes/model/tickets_info.dart';
 import 'package:food_boxes/utility/shared_providers.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
 
 import '../utility/shared_functions.dart';
 import '../utility/size_config.dart';
 
-class SchedulePage extends ConsumerWidget {
+class SchedulePage extends ConsumerStatefulWidget {
   const SchedulePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends ConsumerState<SchedulePage> {
+  late DateTime _selectedDay;
+
+  @override
+  void initState() {
+    _selectedDay = DateTime.now();
+    super.initState();
+  }
+
+  String _fixTimeZone(DateTime time) {
+    final _dateFormater = DateFormat('dd/MM/yyyy');
+    return _dateFormater.format(time);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: DateTime.now(),
-          calendarStyle: CalendarStyle(
-            cellMargin: EdgeInsets.symmetric(
-              vertical: SizeConfig.scaledHeight(1),
+        SizedBox(
+          height: SizeConfig.scaledHeight(55),
+          child: TableCalendar(
+            selectedDayPredicate: (day) {
+              return day == _selectedDay;
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+              });
+            },
+            firstDay: DateTime.utc(2010, 10, 16),
+            lastDay: DateTime.utc(2030, 3, 14),
+            focusedDay: DateTime.now(),
+            calendarStyle: CalendarStyle(
+              cellMargin: EdgeInsets.symmetric(
+                vertical: SizeConfig.scaledHeight(1),
+              ),
             ),
-          ),
-          daysOfWeekStyle: DaysOfWeekStyle(
-            weekdayStyle: Theme.of(context).textTheme.bodySmall!,
-            weekendStyle: Theme.of(context).textTheme.bodySmall!,
-          ),
-          headerStyle: HeaderStyle(
-            formatButtonVisible: false,
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: Theme.of(context).textTheme.bodySmall!,
+              weekendStyle: Theme.of(context).textTheme.bodySmall!,
+            ),
+            headerStyle: HeaderStyle(
+              formatButtonVisible: false,
+            ),
           ),
         ),
         Spacer(),
@@ -38,12 +68,15 @@ class SchedulePage extends ConsumerWidget {
             ref.read(ticketListProvider.notifier).update((state) => [
                   ...state,
                   TicketsInfo(
-                    date: "1/1/23",
+                    date: _fixTimeZone(_selectedDay),
                     name: "Crossant",
                     imageURL: AppConstants.ticketImages[randomValue(0, 7)],
                     price: 5,
                   ),
                 ]);
+            ScaffoldMessenger.of(context).showSnackBar(
+              messegeSnackBar("Ticket successfully created."),
+            );
           },
           child: Text(
             "Create Ticket",

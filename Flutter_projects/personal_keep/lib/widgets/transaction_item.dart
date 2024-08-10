e1a15782c86821888817074f5831677bc0fb32d7
MrJoshCode <jcborut@gmail.com>
2024-08-10 17:40:49 -0400

    Fixed asset bug & completed transaction item widget

--- Flutter_projects/personal_keep/lib/widgets/transaction_item.dart
+++ Flutter_projects/personal_keep/lib/widgets/transaction_item.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:personal_keep/app_constants.dart';
 
 import '../models/transaction.dart';
@@ -20,10 +21,11 @@ class TransactionItem extends StatefulWidget {
 }
 
 class _TransactionItemState extends State<TransactionItem> {
-  late final Color bgColor;
+  late final Color _bgColor;
+
   @override
   void initState() {
-    bgColor = AppConstants.availableColors[Random().nextInt(
+    _bgColor = AppConstants.availableColors[Random().nextInt(
       AppConstants.availableColors.length,
     )];
     super.initState();
@@ -31,7 +33,57 @@ class _TransactionItemState extends State<TransactionItem> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Finish this
-    return const Placeholder();
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: _bgColor,
+          radius: 30,
+          child: Padding(
+            padding: EdgeInsets.all(6),
+            child: FittedBox(
+              child: Text(
+                "\$${widget.transaction.amount}",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          widget.transaction.title,
+          style: TextStyle(
+            fontFamily: "OpenSans",
+          ),
+        ),
+        subtitle: Text(
+          DateFormat.yMMMd().format(
+            widget.transaction.date!,
+          ),
+        ),
+        trailing: MediaQuery.of(context).size.width > 460
+            ? TextButton.icon(
+                onPressed: () => widget.deleteTx(widget.transaction.id),
+                icon: Icon(Icons.delete),
+                label: Text(
+                  "Delete",
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+              )
+            : IconButton(
+                onPressed: () => widget.deleteTx(widget.transaction.id),
+                icon: Icon(Icons.delete),
+                color: Colors.red,
+              ),
+      ),
+    );
   }
 }
